85b3e2f485226cbb25d6cf6bd41f943d3ec11be1
--add <nastase4ka@mail.ru>
2023-03-27 21:43:21 +0300

    modified class Sniper

--- src/main/java/org/example/units/Sniper.java
+++ src/main/java/org/example/units/Sniper.java
@@ -1,2 +1,9 @@
-package org.example.units;public class Sniper {
+package org.example.units;
+
+public class Sniper extends BaseHero{
+    protected int accuracy;
+    public Sniper(String name, int x, int y) {
+        super(name, x, y, 100, new int[]{4, 7}, 15, 4, "Снайпер", 10);
+        accuracy = 70;
+    }
 }
