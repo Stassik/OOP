d06c351a0b9351a66cb3039afd2ec9f8f16ee5d0
--add <nastase4ka@mail.ru>
2023-03-27 21:41:31 +0300

    modified class Armsman

--- src/main/java/org/example/units/Armsman.java
+++ src/main/java/org/example/units/Armsman.java
@@ -1,2 +1,9 @@
-package org.example.units;public class Armsman {
+package org.example.units;
+
+public class Armsman extends  BaseHero{
+    //Непонятно откуда беруться у него стрелы и как он их передает... Он куда-то за ними ходит и потом должен дойти до лучника чтобы передать
+    //или есть какой-то запас этих стрел и они передаются "по воздуху"?
+    public Armsman(String name, int x, int y) {
+        super(name, x, y, 100, new int[]{1, 3}, 0, 1, "Оруженосец", 0);
+    }
 }
