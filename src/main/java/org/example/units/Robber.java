7898ee3af79359dba6f07ce63e42263df04352bd
--add <nastase4ka@mail.ru>
2023-03-27 21:43:04 +0300

    modified class Robber

--- src/main/java/org/example/units/Robber.java
+++ src/main/java/org/example/units/Robber.java
@@ -1,2 +1,8 @@
-package org.example.units;public class Robber {
+package org.example.units;
+
+public class Robber extends BaseHero{
+    public Robber(String name, int x, int y) {
+        super(name, x, y, 100, new int[]{6, 11}, 20, 1, "Разбойник", 0);
+        maxReserve = 20;
+    }
 }
