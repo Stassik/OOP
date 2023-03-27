0d0b32a9be516f8d3ad48130d778138f1e199a66
--add <nastase4ka@mail.ru>
2023-03-27 21:41:08 +0300

    modified class Arbalester

--- src/main/java/org/example/units/Arbalester.java
+++ src/main/java/org/example/units/Arbalester.java
@@ -1,9 +1,9 @@
 package org.example.units;
 
-public class Sniper extends BaseHero{
+public class Arbalester extends BaseHero{
     protected int accuracy;
-    public Sniper(String name, int x, int y) {
-        super(name, x, y, 30, new int[]{4, 7}, 15, 4, "Снайпер", 10);
+    public Arbalester(String name, int x, int y) {
+        super(name, x, y, 100, new int[]{4, 7}, 15, 4, "Арбалетчик", 10);
         accuracy = 70;
     }
 }
