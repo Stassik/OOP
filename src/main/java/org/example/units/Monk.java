43e044913bc04cbae3751393585be1025969f5d3
--add <nastase4ka@mail.ru>
2023-03-27 21:42:46 +0300

    modified class Monk

--- src/main/java/org/example/units/Monk.java
+++ src/main/java/org/example/units/Monk.java
@@ -2,18 +2,13 @@ package org.example.units;
 
 import org.example.Spells;
 
-import java.util.ArrayList;
-
-public class Mage extends BaseHero{
-    protected int manaVol, maxManaVol;    // Запас маны
+public class Monk extends BaseHero{
     protected Spells[] spellsBook = new Spells[3];
-
-
-    public Mage(String name, int x, int y) {
-        super(name, x, y, 100, new int[]{2, 5}, 10, 4, "Маг", 15);
+    public Monk(String name, int x, int y) {
+        super(name, x, y, 100, new int[]{2, 5}, 10, 4, "Монах", 15);
         spellsBook[0] = new Spells("Фаербол");
-        spellsBook[1] = new Spells("Яд");
-        spellsBook[2] = new Spells("Защита");
+        spellsBook[1] = new Spells("Лечение");
+        spellsBook[2] = new Spells("Увеличение силы духа");
 
     }
 }
