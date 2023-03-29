0f6e9c3b887efff069538eded2f9cab20506943c
--add <nastase4ka@mail.ru>
2023-03-29 23:54:44 +0300

    modified class Arbalester

--- src/main/java/org/example/units/Arbalester.java
+++ src/main/java/org/example/units/Arbalester.java
@@ -1,9 +1,7 @@
 package org.example.units;
 
-public class Arbalester extends BaseHero{
-    protected int accuracy;
+public class Arbalester extends Shooter{
     public Arbalester(String name, int x, int y) {
-        super(name, x, y, 100, new int[]{4, 7}, 15, 4, "Арбалетчик", 10);
-        accuracy = 70;
+        super(name, x, y, 100, new int[]{4, 7}, 15, 4, "Арбалетчик", 10, 70);
     }
 }
