e9173b0a786ba308fd4f34aa94d5b693e7bc2908
unknown <nastase4ka@mail.ru>
2023-04-03 23:05:48 +0300

    modified Class Shooter

--- src/main/java/org/example/units/Shooter.java
+++ src/main/java/org/example/units/Shooter.java
@@ -1,5 +1,10 @@
 package org.example.units;
 
+import org.example.GameInterface;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public abstract class Shooter extends BaseHero{
     int accuracy;
     public Shooter(int x, int y, float hp, int[] damage, int def, int distance, String className, int reserve, int accuracy) {
@@ -9,9 +14,42 @@ public abstract class Shooter extends BaseHero{
     }
 
     @Override
-    public void step() {
-        if (hp <= 0 || reserve <= 0) return;
+    public void step(ArrayList<BaseHero> enemy) {
+        if (this.hp <= 0) {
+            return;
+        } else if (this.reserve <= 0 && this.hp > 0) {
+            this.x ++;
+            System.out.println("Игрок: " + this.getInfo());
+        } else {
+            BaseHero unit = findEnemy(enemy);
+            if (unit != null) {
+                shooting(unit);
+            } else {
+                this.x ++;
+            }
+            System.out.println("Игрок: " + this.getInfo());
+            System.out.println("Игрок: " + unit.getInfo());
+        }
+    }
 
+    private void shooting(BaseHero unit) {
+        System.out.println("ПИФ-Паф!");
+        int[] crash = chooseDamage();
+        unit.hp = unit.hp * (1 + unit.def/100) - new Random().nextInt(crash[0], crash[1]);
+    }
+
+    // Если запаса стрел больше половины то можно использовать критический удар. Запас стрел тогда уменьшится на 5 поинтов.
+    private int[] chooseDamage () {
+        if (reserve >= maxReserve/2) {
+            int[] criticalDamage = new int[] {this.damage[0]*2, this.damage[1]*2};
+            System.out.println("Нанесен критический урон!");
+            this.reserve -= 5;
+            return criticalDamage;
+        } else {
+            this.reserve--;
+            return this.damage;
+
+        }
     }
 
 
