a35663fce7937f948c732e1a074298436a391fe6
unknown <nastase4ka@mail.ru>
2023-04-11 23:42:19 +0300

    modified project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,18 +17,30 @@ public class Main {
         darkTeam = BaseHero.generateTeam(2);
 
         Scanner input = new Scanner(System.in);
-        while (true) {
+        while ( BaseHero.checkedTeam(holyTeam) && BaseHero.checkedTeam(darkTeam)) {
+//        while (true) {
             allTeam = BaseHero.sortedPriority(darkTeam, holyTeam);
             View.view();
-            input.nextLine();
+//            input.nextLine();
             for (BaseHero unit : allTeam) {
                 if (holyTeam.contains(unit)) {
+                    System.out.println("Игрок: " + unit.toString() + " | Ближайший враг: " + unit.findEnemy(holyTeam).toString());
                     unit.step(darkTeam, holyTeam);
                 } else {
                     System.out.println("Игрок: " + unit.toString() + " | Ближайший враг: " + unit.findEnemy(holyTeam).toString());
                     unit.step(holyTeam, darkTeam);
                 }
             }
+            for (BaseHero u: allTeam) {
+                if (u.status.equals("busy")) {
+                    u.status = "stand";
+                }
+            }
+        }
+        if (BaseHero.checkedTeam(holyTeam)) {
+            System.out.println("Победила команда Света!");
+        } else {
+            System.out.println("Победила команда Тьмы!");
         }
 
 
